89c38480fe4325f4810977ff6b83e5a0419ce2af
manuel <wither.skel@protonmail.me>
2026-02-28 22:41:53 +0100

    Edit some typos

--- rn_game.py
+++ rn_game.py
@@ -3,7 +3,7 @@ from random import randrange
 print("Welcome to the Random Number Guessing Game!"
       "I will think of a number between 0 and 100, and you have to guess it. ")
 
-gues_this_number = randrange(0, 100)
+guess_this_number = randrange(0, 100)
 print(f"Please select the difficulty level: " 
     "1. Easy (10 chances)" 
     "2. Medium (5 chances)" 
@@ -22,14 +22,14 @@ else:
 try:
     for i in range(difficulty):
         guess = int(input("Guess a number between 0 and 100: "))
-        if guess > gues_this_number:
+        if guess > guess_this_number:
             print(f"The number is lower than {guess}")
-        elif guess < gues_this_number:
+        elif guess < guess_this_number:
             print(f"The number is higher than {guess}")
-        elif guess == gues_this_number:
-            print(f"You guessed the correct number wich was: {gues_this_number}.")
+        elif guess == guess_this_number:
+            print(f"You guessed the correct number which was: {guess_this_number}.")
             break
         else:
-            print(f"Wrong guess, the number was : {gues_this_number}")
+            print(f"Wrong guess, the number was : {guess_this_number}")
 except NameError:
-    print("diffeculty level not set, please choose a valid difficulty level")
+    print("Difficulty level not set, please choose a valid difficulty level")
